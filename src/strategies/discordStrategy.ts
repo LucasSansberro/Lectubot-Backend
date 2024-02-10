f1f02544fd1aa09134ab26bf5acf9e7c8f7b23f4
Lucas Sansberro <lucas_sansberro@outlook.com>
2024-02-10 14:02:58 -0300

    added upsert to login in passport

--- src/strategies/discordStrategy.ts
+++ src/strategies/discordStrategy.ts
@@ -1,8 +1,7 @@
-import { Model } from "mongoose";
 import passport from "passport";
 import { Strategy } from "passport-discord";
 import ENV from "../config.js";
-import {User} from "../models/Schemas/User.js";
+import { User } from "../models/Schemas/User.js";
 
 const { CLIENTID, CLIENTSECRET } = ENV;
 type User = {
@@ -10,10 +9,6 @@ type User = {
 };
 passport.serializeUser((user: User, done) => {
   done(null, user._id);
-});
-
-passport.deserializeUser(async (user: User, done) => {
-  done(null, user);
 });
 
 passport.use(
@@ -26,23 +21,20 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        const user = await User.findOne({ discordId: profile.id });
-        if (user) {
-          return done(null, user);
-        }
-        console.log(profile);
+        //TODO Don't save guild in DB, just check if user is in guild = lectores 
+        const user = await User.findOneAndUpdate(
+          { discordId: profile.id },
+          {
+            $set: {
+              username: profile.username,
+              avatar: profile.avatar,
+              guilds: profile.guilds,
+            },
+          },
+          { new: true, upsert: true }
+        );
 
-        //TODO Dont upload guilds to schema. Compare LECTORESID to check if user is member
-        const newUser = new User({
-          discordId: profile.id,
-          username: profile.username,
-          avatar: profile.avatar,
-          guilds: profile.guilds,
-        });
-
-        await newUser.save();
-
-        done(null, newUser);
+        return done(null, user);
       } catch (error: Error | any) {
         console.log(error);
         return done(error, undefined);
